83fc04dede9e120a94dded7f023afafba5f127ce
Julianne Soifer <juliannesoifer@gmail.com>
2018-10-05 11:25:18 -0700

    complete testing of first spec pass/fail as expected.

--- spec/space-age-spec.js
+++ spec/space-age-spec.js
@@ -3,8 +3,8 @@ import { getAge } from './../src/business.js';
 describe( 'getAge', function() {
 
   it('should calculate a users age based on inputted birthday', function() {
-    let inputtedBirthday = "1978-10-05";
-    var result = getAge(inputtedBirthday);
-    expect(result).toEqual(40);
+    let inputtedBirthday = "10/05/1978";
+    var age = getAge(inputtedBirthday);
+    expect(age).toBe(40);
   });
 });
--- src/business.js
+++ src/business.js
@@ -3,8 +3,8 @@ export function getAge() {
   let inputtedBirthday = new Date("10/05/1978");
   let today = new Date();
   const timeDiff = Math.abs(today.getTime() - inputtedBirthday.getTime());
-  const diffDays = Math.ciel(timeDiff / (1000 * 3600 * 24));
-  const age = ((diffDays/365).round);
+  const diffDays = (Math.round(timeDiff / (1000 * 3600 * 24)));
+  const age = (Math.round(diffDays/365));
   return age;
 
 }

beab538d30e2e45a3550de370fe283245341ff10
Julianne Soifer <juliannesoifer@gmail.com>
2018-10-05 16:36:49 -0700

    try to test third spec, get errors about unused variables which are used and undefined classes which are defined.

--- spec/space-age-spec.js
+++ spec/space-age-spec.js
@@ -1,11 +1,11 @@
-import { getAge, SpaceAge } from './../src/business.js';
+import { getAge, SpaceAge, planetYear } from './../src/business.js';
 
 describe( 'getAge', function() {
 
   it('should calculate a users age based on inputted birthday', function() {
     let inputtedBirthday = "10/05/1978";
-    var age = getAge(inputtedBirthday);
-    expect(age).toBe(40);
+    var earthAge = getAge(inputtedBirthday);
+    expect(earthAge).toBe(40);
   });
 });
 
@@ -14,6 +14,9 @@ describe( 'SpaceAge', function() {
   it('should calculate a users age in Mercury years', function() {
     let mercury = new SpaceAge(40,.24);
     expect(mercury.planetAge()).toBe(167);
-
+  });
+  it('should calculate a users age in venus years', function() {
+    let venus = new SpaceAge(40,.62);
+    expect(venus.planetAge()).toBe(65);
   });
 });
